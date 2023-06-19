7c929c837e3361f700ccdd1392cf5114f8cecc57
faeze <faezeatwork@gmail.com>
2023-06-19 09:01:04 +0330

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div className="container h-100 d-flex flex-column justify-content-center align-items-center">
+      <div className="bg-success h-50 customForm"></div>
+      <div className="bg-danger  mt-2 customRegister"></div>
+    </div>
+  );
+}
